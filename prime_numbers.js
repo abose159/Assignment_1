0904de97388d1e0e2b19b101eb90d9abff400b5d
Tushar Singh <karansinghrajora1996@gmail.com>
2022-11-30 10:13:55 +0530

    code for palindrome and prime number have been fixed

--- prime_numbers.js
+++ prime_numbers.js
@@ -1,13 +1,13 @@
 //Identifying prime numbers
 
 let num=5;
-let count=1;
+let count=0;
     for (let i=1; i<=num; i++) {
         if(num%i==0) {
-            count+2;
+            count++;
         }
     }
-if(count!==2) {
+if(count==2) {
     console.log("Yes");
 } else {
     console.log("No");
